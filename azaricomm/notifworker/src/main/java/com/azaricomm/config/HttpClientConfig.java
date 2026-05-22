2024cb11c39deaa666cab66d78fe38a7f5d2f5f0
jarivankaam <jari@jarivankaam.nl>
2026-05-22 19:19:44 +0200

    add tsl3 configuration.

--- azaricomm/notifworker/src/main/java/com/azaricomm/config/HttpClientConfig.java
+++ azaricomm/notifworker/src/main/java/com/azaricomm/config/HttpClientConfig.java
@@ -1,14 +1,40 @@
 package com.azaricomm.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.core5.http.ssl.TLS;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class HttpClientConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, null, null);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(
+                        PoolingHttpClientConnectionManagerBuilder.create()
+                                .setSSLSocketFactory(
+                                        SSLConnectionSocketFactoryBuilder.create()
+                                                .setSslContext(sslContext)
+                                                .setTlsVersions(TLS.V_1_3)
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
+
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
     }
 }
