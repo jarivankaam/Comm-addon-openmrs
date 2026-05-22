1cd37aaaf5295b0b3b3c501d00b515ca41d6de3e
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2026-05-22 12:12:28 +0200

    Creation of Idempotacy on appointments webhook

--- azaricomm/api/src/main/java/com/azaricomm/api/model/Appointment.java
+++ azaricomm/api/src/main/java/com/azaricomm/api/model/Appointment.java
@@ -2,6 +2,8 @@ package com.azaricomm.api.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import jakarta.validation.constraints.NotNull;
@@ -9,6 +11,9 @@ import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Document(collection = "appointments")
+@CompoundIndexes({
+        @CompoundIndex(name = "patient_schedule_idx", def = "{'patientId': 1, 'scheduledTime': 1}", unique = true)
+})
 public class Appointment {
 
     @Id
